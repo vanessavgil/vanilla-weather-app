f32cb3897406a9af9f51fc5ae5bccbb9789e2fab
v. v. gil <133064860+vanessavgil@users.noreply.github.com>
2023-07-08 18:21:58 -0400

    dynamic html javascript
    2 columns of dynamic javascript

--- src/app.js
+++ src/app.js
@@ -21,6 +21,50 @@ let days = [
 let day = days[date.getDay()];
 return `${day} ${hours}:${minutes}`;
 }
+
+function displayForecast(){
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecastHTML = forecastHTML + `
+
+  <div class="col-2">
+    <div class="weather-forecast-date">Thurs</div>
+    <img
+      src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+      alt="partly cloudy"
+      width="26px"
+    />
+    <div class="weather-forecast-temperature">
+      <span class="weather-forecast-temp-max">18° </span>
+      <span class="weather-forecast-temp-min">12° </span>
+    </div>
+  </div>
+`; 
+forecastHTML =
+  forecastHTML +
+  `
+  
+  <div class="col-2">
+    <div class="weather-forecast-date">Thurs</div>
+    <img
+      src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+      alt="partly cloudy"
+      width="26px"
+    />
+    <div class="weather-forecast-temperature">
+      <span class="weather-forecast-temp-max">18° </span>
+      <span class="weather-forecast-temp-min">12° </span>
+    </div>
+  </div>
+
+`; 
+forecastHTML = forecastHTML + `</div>`
+  forecastElement.innerHTML = forecastHTML;
+}
+
+
+
 function displayTemperature(response){
     
     let temperatureElement = document.querySelector("#temperature");
@@ -101,3 +145,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
