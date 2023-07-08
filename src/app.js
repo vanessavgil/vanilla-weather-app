6a325fd2d816ddc1114a5d0a0d6571a7620a9c71
v. v. gil <133064860+vanessavgil@users.noreply.github.com>
2023-07-08 00:19:07 -0400

    updated elements
    updated temp, city, description, humidity, wind elements

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 function displayTemperature(response){
     console.log(response.data);
-    console.log(response.data.main.temp);
+    
     let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = response.data.main.temp;
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.name;
+
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML = response.data.weather[0].description;
+
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = response.data.main.humidity;
+
+    let windElement =document.querySelector("#wind");
+    windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 let apiKey = "5f2a54906ddaac5f7865c52af1ed9094";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
