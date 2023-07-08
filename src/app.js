59d40fd3fdc0a6e1b383e79a0b459788c7859d29
v. v. gil <133064860+vanessavgil@users.noreply.github.com>
2023-07-08 11:16:37 -0400

    date first steps

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
+function formatDate(timestamp){
+//calculate the date
+let date = new Date(timestamp);
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let day = date.getDay();
+return `${day} ${hours}:${minutes}`;
+}
 function displayTemperature(response){
     console.log(response.data);
     
@@ -15,6 +23,9 @@ function displayTemperature(response){
 
     let windElement =document.querySelector("#wind");
     windElement.innerHTML = Math.round(response.data.wind.speed);
+
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(response.data.dt*1000);
 }
 let apiKey = "5f2a54906ddaac5f7865c52af1ed9094";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
