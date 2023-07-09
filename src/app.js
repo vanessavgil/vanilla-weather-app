8dd86c87fe1c9fa153a4e608f8c5a3551cd35a54
v. v. gil <133064860+vanessavgil@users.noreply.github.com>
2023-07-08 21:38:44 -0400

    forecastDay and icon

--- src/app.js
+++ src/app.js
@@ -23,26 +23,23 @@ return `${day} ${hours}:${minutes}`;
 }
 
 function displayForecast(response){
-    console.log(response.data.daily);
+let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
-let days = ["Thursday", "Friday", "Saturday", "Sunday", "Monday"];
   let forecastHTML = `<div class="row">`;
   
-  days.forEach(function (day) {
+  forecast.forEach(function (forecastDay) {
     forecastHTML =
       forecastHTML +
-      `
-
-  <div class="col-2">
-    <div class="weather-forecast-date">${day}</div>
+      `<div class="col-2">
+    <div class="weather-forecast-date">${forecastDay.dt}</div>
     <img
-      src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-      alt="partly cloudy"
+      src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
       width="26px"
     />
     <div class="weather-forecast-temperature">
-      <span class="weather-forecast-temp-max">18° </span>
-      <span class="weather-forecast-temp-min">12° </span>
+      <span class="weather-forecast-temp-max"> (${forecastDay.temp.max})° </span>
+      <span class="weather-forecast-temp-min">${forecastDay.temp.min}° </span>
     </div>
   </div>
 `;
